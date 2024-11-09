7c41f1a883c2754b44a9eb1de4696484a625a063
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-11-09 19:02:16 +0200

    Add firebase functionality
    Add firebase methods to save a story and fetch all stories

---
+++ components/firebase/firebaseActions.tsx
@@ -0,0 +1,34 @@
+import { app } from "@/firebaseConfig";
+import { getDatabase, ref, push, onValue } from "firebase/database";
+
+import { Story } from "@/types/story";
+
+const database = getDatabase(app);
+
+export const saveStory = (story: Story) => {
+    if (story.pages && story.name) {
+        push(ref(database, 'stories/'), story);
+    }
+    else {
+        throw new Error('Story must have a name and at least one page');
+    }
+};
+
+export const fetchStories = (): Promise<Story[]> => {
+    return new Promise((resolve, reject) => {
+        const storiesRef = ref(database, 'stories/');
+        onValue(storiesRef, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                const stories: Story[] = Object.entries(data).map(([id, storyData]) => ({
+                    id,
+                    ...storyData as Omit<Story, 'id'>
+                }));
+                resolve(stories);
+            } else {
+                resolve([]);
+            }
+        },
+            (error) => reject(error));
+    })
+};
