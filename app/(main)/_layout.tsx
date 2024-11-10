213a1f17f48b9fa569f4bf9eec4733473043ad11
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-11-10 07:15:58 +0200

    Add custom navigation headers

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,11 +1,16 @@
+import { CustomHeader } from "@/components/CustomHeader";
 import { Stack } from "expo-router";
 
 export default function HomeLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="newStory" />
-      <Stack.Screen name="storyList" />
+      <Stack.Screen name="newStory" options={{
+        header: () => <CustomHeader title="New Story" />
+      }} />
+      <Stack.Screen name="storyList" options={{
+        header: () => <CustomHeader title="Our stories" />
+      }} />
     </Stack>
   );
 }
---
+++ types/customheaderprops.tsx
@@ -0,0 +1,3 @@
+export type CustomHeaderProps = {
+    title: string;
+  };
