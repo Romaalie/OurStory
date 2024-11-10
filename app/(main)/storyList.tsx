acaf75af3cefc2458dfdeae932fb06ac98f41373
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-11-10 08:22:55 +0200

    Add flatlist and functionality to storyList

--- app/(main)/storyList.tsx
+++ app/(main)/storyList.tsx
@@ -1,11 +1,45 @@
-import { Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { fetchStories } from '@/components/firebase/firebaseActions';
+import { Story } from '@/types/story';
+import { useEffect, useState } from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
 
 export default function StoryListScreen() {
+
+  const [stories, setStories] = useState<Story[]>([]);
+
+  useEffect(() => {
+    const getStories = async () => {
+      try {
+        const stories = await fetchStories();
+        setStories(stories);
+      }
+      catch (error) {
+        console.error("Error fetching stories: ", error)
+      }
+    };
+
+    getStories();
+  }, []);
+
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>This is the screen for the listing of a user's stories</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <FlatList
+        ListHeaderComponent={<Text>This is a placeholder text to identify the page when flist is empty</Text>}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) =>
+          <View>
+            <TouchableOpacity>
+              <Text>Click here to view your story: {item.name}</Text>
+            </TouchableOpacity>
+          </View>
+
+        }
+        data={stories}
+      />
+    </View>
   );
 }
 
