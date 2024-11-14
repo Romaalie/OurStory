1623be8b388c56d0f9133c4ef5c79aacb9c9dfe9
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-11-14 10:10:39 +0200

    Update _layout for fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,28 @@
 import { Stack } from "expo-router";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  const [loaded, error] = useFonts({
+    'ShadowsIntoLight-Regular': require('../assets/fonts/ShadowsIntoLight-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+
   return (
       <SafeAreaProvider>
         <Stack>
